8dd9be22cc30cf2d35b4bc8c82eefe0f57e5c8b8
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2022-01-21 13:27:49 -0300

    pagina de explorar

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,22 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import ProfileLink from '../components/ProfileLInk';
+import bannergif from '../images/banner.gif';
+import iconexplorefood from '../images/iconexplorefood.png';
+import iconexploredrink from '../images/iconexploredrink.png';
+import '../styles/explore.css';
 
 export default function Explore() {
+  const history = useHistory();
+
   return (
-    <>
+    <div className="main-div-explore">
+      <img src={ bannergif } alt="banner gif" />
       <header>
-        <ProfileLink />
-        <h1 data-testid="page-title">Explorar</h1>
-        <Link to="/explorar/comidas">
-          <button type="button" data-testid="explore-food">Explorar Comidas</button>
-        </Link>
-        <Link to="/explorar/bebidas">
-          <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
-        </Link>
+        <div className="user-infos-explore">
+          <ProfileLink />
+          <h1 data-testid="page-title">Explorar</h1>
+        </div>
       </header>
+      <div className="links-container">
+        <button
+          type="button"
+          onClick={ () => history.push('/explorar/comidas') }
+          data-testid="explore-food"
+        >
+          <div className="btn-infos-explore">
+            <h2>Explorar comida</h2>
+            <img src={ iconexplorefood } alt="food" />
+          </div>
+        </button>
+        <button
+          onClick={ () => history.push('/explorar/bebidas') }
+          type="button"
+          data-testid="explore-drinks"
+        >
+          <div className="btn-infos-explore">
+            <h2>Explorar bebida</h2>
+            <img src={ iconexploredrink } alt="drink" />
+          </div>
+        </button>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
